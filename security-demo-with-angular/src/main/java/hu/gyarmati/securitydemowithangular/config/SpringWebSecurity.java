24e0b54ee677d1eae1aa54fd5f8b51416de5636f
gyarmatiadam <gyari12@gmail.com>
2024-01-13 14:34:16 +0100

    config files update

--- security-demo-with-angular/src/main/java/hu/gyarmati/securitydemowithangular/config/SpringWebSecurity.java
+++ security-demo-with-angular/src/main/java/hu/gyarmati/securitydemowithangular/config/SpringWebSecurity.java
@@ -29,7 +29,7 @@ public class SpringWebSecurity implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("ec2-3-70-135-81.eu-central-1.compute.amazonaws.com")
+                .allowedOrigins("http://ec2-3-70-135-81.eu-central-1.compute.amazonaws.com")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*");
     }
